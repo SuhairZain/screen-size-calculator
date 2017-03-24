ea270f21e7ad48fe435ac523da32b5392c8d135e
Suhair Zain <suhair@qplaytech.com>
2017-03-24 17:52:13 +0530

    Revert "Revert "First working version""
    This reverts commit 3543db54295575c8f938bbe1167d6bb369a077b8.

--- src/App.jsx
+++ src/App.jsx
@@ -5,25 +5,98 @@ import {
     COLOR_TEXT
 } from './utils/colors.js'
 
+import InputField from './InputField.jsx';
+
 const styles = {
     root: {
         alignItems: 'center',
         backgroundColor: COLOR_BG,
         display: 'flex',
-        flexDirection: 'column',
-        height: '100%',
+        flex: 1,
         justifyContent: 'center',
-        width: '100%'
+        padding: 24,
     },
-    title: {
-        color: COLOR_TEXT
-    }
+    screen: {
+        backgroundColor: 'white',
+    },
+    fields: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'space-around',
+        width: '50%',
+    },
+    inputField: {
+        marginBottom: 24,
+        width: '40%',
+    },
 };
 
-const App = () => (
-    <div style={styles.root}>
-        <span style={styles.title}>Hello React!</span>
-    </div>
-);
+const square = (num) => num * num;
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            diagonalInches: '9.7',
+            heightPixels: '1024',
+            widthPixels: '768',
+        };
+    }
+
+    setValue = (type, value) => {
+        this.setState({
+            ...this.state,
+            [type]: value,
+        });
+    }
+
+    render() {
+        const { heightPixels, widthPixels, diagonalInches } = this.state;
+
+        const hp = parseInt(heightPixels);
+        const wp = parseInt(widthPixels);
+        const dI = parseFloat(diagonalInches);
+        const diagonalPixels = Math.sqrt(square(hp) + square(wp));
+        const denstity = dI / diagonalPixels;
+
+        const heightInches = denstity * heightPixels;
+        const widthInches = denstity * widthPixels;
+
+        return (
+            <div style={styles.root}>
+                <div style={{
+                    ...styles.screen,
+                    height: `${heightInches}in`,
+                    width: `${widthInches}in`,
+                }} />
+                <div style={styles.fields}>
+                    <InputField
+                        style={styles.inputField}
+                        title="Width (Pixels)"
+                        text={widthPixels}
+                        onChange={(val) => this.setValue('widthPixels', val)} />
+                    <InputField
+                        style={styles.inputField}
+                        title="Height (Pixels)"
+                        text={heightPixels}
+                        onChange={(val) => this.setValue('heightPixels', val)} />
+                    <InputField
+                        style={styles.inputField}
+                        title="Width (Inches)"
+                        text={widthInches.toString()} />
+                    <InputField
+                        style={styles.inputField}
+                        title="Height (Inches)"
+                        text={heightInches.toString()} />
+                    <InputField
+                        style={styles.inputField}
+                        title="Diagonal (Inches)"
+                        text={diagonalInches}
+                        onChange={(val) => this.setValue('diagonalInches', val)} />
+                </div>
+            </div>
+        );
+    }
+}
 
 export default App;
